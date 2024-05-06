777390817a3903dcc56289141ac382ed5e855ef3
E1B53 <u11202253@o365.ttu.edu.tw>
2024-05-07 01:34:52 +0800

    這個印出三角形的程式對我來說比較陌生以前沒試過 在打程式的過程中我使用了呼叫函數的指令，因為我覺得一次打在裡面要對括號很麻煩所以我拉出來印三角形的程式 我也使用了getchar來清除我的殘留值

--- hw2.c
+++ hw2.c
@@ -63,24 +63,71 @@ int main()
 	puts("///////////////////////////////////////");
 	puts("/   E                        E        /");
 	puts("/    1    a=畫出直角三角形    1       /");
-	puts("/     B    b=顯示乘法表        B      /");
-	puts("/      5   c=結束               5     /");
+	puts("/     B   b=顯示乘法表         B      /");
+	puts("/      5  c=結束                5     /");
 	puts("/       3                        3    /");
 	puts("///////////////////////////////////////");
-//4
+//4	
+	char ch,tri,nineXnine;
+	printf("請輸入一個字元在a到c之間:\n");
+    scanf(" %c", &ch);
+		getchar();
+	switch (ch){
+    	case 'A':
+    	case 'a':	
+		    system("cls");
+       	    printf("請輸入一個(a-n)的字母:");
+       		scanf("%c",&tri);
+			while (tri<'a'||tri>'n')
+       		{
+       			printf("錯誤，輸入字元不在'a'至'n'區間\n");
+       			printf("請重新輸入一個(a-n)的字母:");
+       			scanf("%c",&tri);
+       			getchar();
+       		}
+       	Triangle(tri);
+		break;
+			
+				
+    	
+    	
+		
+		
+		
+		
+		
+		
+		
+       		
 
 
+        
+        
+        
+       	
+       	
+    
+}
+system("pause");
+	return 0;
+}
 
+void Triangle(char tri){   
+    int i,j;
+    int rows=tri-'a'+1; 
 
+    for (i=0; i<rows;i++){
+       
+        for (j=rows-i-1;j>0;j--){
+            printf(" ");
+        }
+     
+        for (j=0;j<=i;j++){
+            printf("%c",'a'+j);
+        }
+        printf("\n");
+    }
+}
 
+	
 
-
-
-
-
-
-
-
-
-
-} 
